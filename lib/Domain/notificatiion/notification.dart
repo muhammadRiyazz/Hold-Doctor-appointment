aaa43bb58ca15066257b6bb5d5f4eca3e23bed11
Muhammad Riyaz <riyazmuhammad786rz@gmail.com>
2023-02-04 23:17:59 +0530

    notification

--- lib/Domain/notificatiion/notification.dart
+++ lib/Domain/notificatiion/notification.dart
@@ -3,14 +3,34 @@ import 'dart:developer';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class Notificatoinclass {
-  static final mynotification = FlutterLocalNotificationsPlugin();
+  final mynotification = FlutterLocalNotificationsPlugin();
+  void init() {
+    final AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings('app_icon');
 
-  static Future shownotification({
-    int id = 0,
-    String? title,
-    String? body,
-    String? payload,
-  }) async {
+    final DarwinInitializationSettings initializationSettingsIOS =
+        DarwinInitializationSettings(
+      requestSoundPermission: false,
+      requestBadgePermission: false,
+      requestAlertPermission: false,
+      onDidReceiveLocalNotification: onDidReceiveLocalNotification,
+    );
+
+    final InitializationSettings initializationSettings =
+        InitializationSettings(
+      android: initializationSettingsAndroid,
+      iOS: initializationSettingsIOS,
+    );
+    mynotification.initialize(initializationSettings,
+        onDidReceiveNotificationResponse: onDidReceiveNotificationResponse);
+  }
+
+  Future shownotification(
+      {int id = 0,
+      String? title,
+      String? body,
+      String? payload,
+      mynotification}) async {
     log('fghjkl');
 
     mynotification.show(
@@ -25,8 +45,22 @@ class Notificatoinclass {
   static Future notificationDetails() async {
     log('fgh');
     return const NotificationDetails(
-      android: AndroidNotificationDetails('', '', importance: Importance.max),
+      android: AndroidNotificationDetails(
+        '',
+        '',
+        importance: Importance.max,
+        icon: 'cvb',
+      ),
       iOS: DarwinNotificationDetails(),
     );
+  }
+
+  void onDidReceiveLocalNotification(
+      int id, String? title, String? body, String? payload) {
+    print('');
+  }
+
+  void onDidReceiveNotificationResponse(NotificationResponse details) {
+    print('object');
   }
 }
